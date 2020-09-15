ecab3e4d83d19c11d97965b841aae77d3fb27556
AlexTnt <alextemplartnt@gmail.com>
2020-09-15 17:28:09 +0300

    server-ts simple types enum

---
+++ server-ts/dist/file/basic-type-array.enum.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BasicTypeArrayEnum = void 0;
+var BasicTypeArrayEnum;
+(function (BasicTypeArrayEnum) {
+    BasicTypeArrayEnum["BOOLEAN"] = "boolean[]";
+    BasicTypeArrayEnum["NUMBER"] = "number[]";
+    BasicTypeArrayEnum["STRING"] = "string[]";
+    BasicTypeArrayEnum["ANY"] = "any[]";
+    BasicTypeArrayEnum["SYMBOL"] = "symbol[]";
+    BasicTypeArrayEnum["GENERIC_BOOLEAN"] = "Array<boolean>";
+    BasicTypeArrayEnum["GENERIC_NUMBER"] = "Array<number>";
+    BasicTypeArrayEnum["GENERIC_STRING"] = "Array<string>";
+    BasicTypeArrayEnum["GENERIC_ANY"] = "Array<any>";
+    BasicTypeArrayEnum["GENERIC_SYMBOL"] = "Array<sumbol>";
+})(BasicTypeArrayEnum = exports.BasicTypeArrayEnum || (exports.BasicTypeArrayEnum = {}));
+//# sourceMappingURL=basic-type-array.enum.js.map
---
+++ server-ts/src/file/basic-type-array.enum.ts
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-shadow
+export enum BasicTypeArrayEnum {
+  BOOLEAN = 'boolean[]',
+  NUMBER = 'number[]',
+  STRING = 'string[]',
+  ANY = 'any[]',
+  SYMBOL = 'symbol[]',
+  GENERIC_BOOLEAN = 'Array<boolean>',
+  GENERIC_NUMBER = 'Array<number>',
+  GENERIC_STRING = 'Array<string>',
+  GENERIC_ANY = 'Array<any>',
+  GENERIC_SYMBOL = 'Array<sumbol>'
+}
